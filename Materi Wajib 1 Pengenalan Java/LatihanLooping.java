04dbcd67c4122ff3f2d113cfa172609e01132edc
alifsuryadi <alifsuryadi037@gmail.com>
2022-12-04 10:39:36 +0700

    Menambahkan tugas baru

---
+++ Materi Wajib 1 Pengenalan Java/LatihanLooping.java
@@ -0,0 +1,19 @@
+public class LatihanLooping {
+    
+    public static void main(String[] args){
+        int hurufa = 0;
+        String kalimat = "Saya senang sekali";
+
+        //kalimat.length = kalimat dihitung satu-satu
+        for(int i = kalimat.length()- 1; i >= 0 ; i--){
+
+            //charAt = Mengambil satu huruf
+            if(kalimat.charAt(i)== 'a'){
+                hurufa++;
+            }
+        }
+
+        System.out.println("Jumlah Huruf a : " + hurufa);
+    }
+
+}
