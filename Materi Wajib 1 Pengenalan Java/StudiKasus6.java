9a4bf4c8a8415b951a63e7f383505a5b040a6d3c
alifsuryadi <alifsuryadi037@gmail.com>
2022-12-04 11:53:43 +0700

    Menambahkan tugas baru

---
+++ Materi Wajib 1 Pengenalan Java/StudiKasus6.java
@@ -0,0 +1,14 @@
+public class StudiKasus6 {
+    
+    public static void main(String[] args){
+        String kalimat1 = "budi";
+        String kalimat2 = "BUDI";
+        
+        if(kalimat1.equals(kalimat2)){
+            System.out.println(true);
+        }
+        else{
+            System.out.println(false);
+        }
+    }
+}
