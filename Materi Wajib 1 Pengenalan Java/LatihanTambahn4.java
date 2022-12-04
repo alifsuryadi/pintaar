98519646c099e6e98b4c89d435471b592620433b
alifsuryadi <alifsuryadi037@gmail.com>
2022-12-04 14:22:32 +0700

    Tambah file latihan baru

---
+++ Materi Wajib 1 Pengenalan Java/LatihanTambahn4.java
@@ -0,0 +1,13 @@
+public class LatihanTambahn4 {
+    
+        public static void main(String[] args){
+            int n = 20;
+            if (n % 2 == 0) {
+                System.out.println(1);
+            }
+        else {
+                System.out.println(0);
+            }
+        }
+    
+}
