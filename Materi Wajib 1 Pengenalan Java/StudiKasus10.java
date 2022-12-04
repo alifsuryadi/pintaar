5c65a6ae8d5f20eaac41f72217dff932ed0c5206
alifsuryadi <alifsuryadi037@gmail.com>
2022-12-04 12:18:47 +0700

    Menambahkan file tugas

---
+++ Materi Wajib 1 Pengenalan Java/StudiKasus10.java
@@ -0,0 +1,15 @@
+public class StudiKasus10 {
+    public static void main(String[] args){
+        int a = 82;
+
+        //program sederhana
+        if(a < 70){
+            System.out.println("Nilai kamu : "+a);
+            System.out.println("Kamu belum lulus");
+        }
+        else{
+            System.out.println("Nilai kamu : "+a);
+            System.out.println("Selamat kamu lulus");
+        }
+    }
+}
