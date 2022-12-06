2077243d9ae50689e9eb4f315e35d33f536a46cf
alifsuryadi <alifsuryadi037@gmail.com>
2022-12-06 22:42:21 +0700

    Change my program

--- Materi_Wajib_2_OOP/ClassPesawat.java
+++ Materi_Wajib_2_OOP/ClassPesawat.java
@@ -1,45 +1,89 @@
 package Materi_Wajib_2_OOP;
 
+import java.util.Scanner;
+
 public class ClassPesawat {
-    int KapasitasPenumpang = 10;
-    int TahunProduksi = 2015;
-    int JumlahPenerbangan = 4;
-    String NamaMaskapai = "Alif";
-    String DataMasihLayarPakai = "Layak";
+    String NamaMaskapai;
+    int KapasitasPenumpang;
+    int JumlahPenerbangan;
+    int TahunProduksi;
+    boolean MasihLayakPakai;
 
-    public void UpdatePenerbangan(){
-        JumlahPenerbangan++;
+    public ClassPesawat(String NamaMaskapai,
+                        int KapasitasPenumpang,
+                        int TahunProduksi ){
+
+    this.NamaMaskapai = NamaMaskapai;
+    this.KapasitasPenumpang = KapasitasPenumpang;
+    this.TahunProduksi = TahunProduksi;
+    this.MasihLayakPakai = true;
+    this.JumlahPenerbangan = 0;
+
     }
 
-    public boolean CekKapasitasPenumpang(){
-        if(JumlahPenerbangan < 50){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
+        public boolean terbang(int KapasitasPenumpangTerbang){
 
-    public boolean BatasPenerbangan(){
-        if(JumlahPenerbangan < 5 || TahunProduksi > 2014){
-            System.out.println("Data Masih Layak Pakai");
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
+            if(MasihLayakPakai && KapasitasPenumpang >=
+                KapasitasPenumpangTerbang && TahunProduksi > 2014){
 
-    public static void main(String[] args){
-        ClassPesawat pesawat = new ClassPesawat();
+                    this.JumlahPenerbangan++;
+                    if(JumlahPenerbangan > 5){
+                        MasihLayakPakai = false;
+                    }
+                    return true;
+                }
+                else{
+                    return false;
+                }
+        }
+
+        public int getJumlahKapasitasPenumpang(){
+            return KapasitasPenumpang;
+        }
+        public boolean getMasihLayakPakai(){
+            return MasihLayakPakai;
+        }
+        public int getJumlahPenerbangan(){
+            return JumlahPenerbangan;
+        }
+        public int getTahunProduksi(){
+            return TahunProduksi;
+        }
+
     
-        System.out.println("Kapasitas Penumpang : "+pesawat.KapasitasPenumpang);
-        System.out.println("Tahun Diproduksi    : "+pesawat.TahunProduksi);
-        System.out.println("Jumlah Penerbangan  : "+pesawat.JumlahPenerbangan);
-        pesawat.UpdatePenerbangan();
-        System.out.println("Jumlah Penerbangan  : "+pesawat.JumlahPenerbangan);
-        System.out.println("Nama Maskapai       : "+pesawat.NamaMaskapai);
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukan Nama Maskapai : " );
+        String NamaMaskapai = in.nextLine();
+        System.out.print("Masukan Kapasitas Penumpang : ");
+        int KapasitasPenumpang =in.nextInt();
+        System.out.print("Masukan Tahun Produksi : ");
+        int TahunProduksi = in.nextInt();
 
-        System.out.println("Batas Penerbangan    : "+pesawat.BatasPenerbangan());
+        ClassPesawat classPesawat = new ClassPesawat(NamaMaskapai, KapasitasPenumpang, TahunProduksi);
+
+
+        //hasNextLine = input tidak terbatas
+        while(in.hasNextLine()){
+            System.out.println("Apakah pesawat akan terbang? (y/n) : ");
+
+            if(in.nextLine().equals("y")){
+                System.out.print("Berapa penumpang akan terbang? : ");
+                int KapasitasPenumpangTerbang = in.nextInt();
+
+                if(classPesawat.terbang(KapasitasPenumpangTerbang)){
+                    System.out.println("Pesawat Terbang");
+                }
+                else{
+                    System.out.println("Pesawat Tidak Boleh Terbang Lagi");
+                }
+            }
+
+        }
+
+        in.close();
     }
+
+    
+
 }
